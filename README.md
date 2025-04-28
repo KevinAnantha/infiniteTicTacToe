1c9383d1c2b3d68d0a8f65638996d1ff63fd190d
KevinAnantha <kevinanantha@students.amikom.ac.id>
2025-04-29 00:00:29 +0700

    fix double click first turn

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 3x3 infinite tic tac TicTacToe
 
 coordinates : 
+
 ![Logo](https://kevinanantha.github.io/infiniteTicTacToe/coordinates.png)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let turn = "silang"
 let dataLingkaran = []
 let dataSilang = []
 let win = false
+// let timer = 5;
 
 document.getElementById('turn').innerText = `Giliran : ${turn}`
 
@@ -16,7 +17,7 @@ function main(x, y){
 
     let idTarget = hurufX + hurufY
 
-    if(dataSilang.length > 0 && turn == "silang"){
+    if(dataSilang.length >= 0 && turn == "silang"){
         for(id of dataSilang){
             if(id == idTarget){
                 return
@@ -27,7 +28,7 @@ function main(x, y){
                 return
             }
         }
-    }else if(dataLingkaran.length > 0 && turn == "lingkaran"){
+    }else if(dataLingkaran.length >= 0 && turn == "lingkaran"){
         for(id of dataSilang){
             if(id == idTarget){
                 return
@@ -44,65 +45,95 @@ function main(x, y){
     // looping array data lingkaran dan data silang, apakah ada dalam array tersebut
 
     let targetDiv = document.getElementById(idTarget)
-    targetDiv.classList.add(turn)
+    targetDiv.classList.add(turn) // add style (simbol) ke koordinat yang sudah dipilih
 
     if(turn == "silang"){
         if(dataSilang.length === 3){
             let targetHapus = dataSilang[0]
             document.getElementById(targetHapus).classList.remove(turn)
-            document.getElementById(targetHapus).style.opacity = "1"
+            // document.getElementById(targetHapus).style.opacity = "1"
+            document.getElementById(targetHapus).classList.remove("bg-danger")
+            document.getElementById(targetHapus).classList.remove("bg-warning")
             dataSilang.shift()
             dataSilang.push(idTarget)
             if(winning()){
                 return
             }
             let idFade = dataSilang[0]
-            document.getElementById(idFade).style.opacity = "0.5"
+            let idFade2 = dataSilang[1]
+            // document.getElementById(idFade).style.opacity = "0.3"
+            document.getElementById(idFade).classList.add("bg-danger")
+            // document.getElementById(idFade2).style.opacity = "0.5"
+            document.getElementById(idFade2).classList.add("bg-warning")
         }else if(dataSilang.length === 2){
             dataSilang.push(idTarget)
             if(winning()){
                 return
             }
             let idFade = dataSilang[0]
-            document.getElementById(idFade).style.opacity = "0.5"
-            targetDiv.classList.add(turn)
+            let idFade2 = dataSilang[1]
+            // document.getElementById(idFade).style.opacity = "0.3"
+            document.getElementById(idFade).classList.add("bg-danger")
+            // document.getElementById(idFade2).style.opacity = "0.5"
+            document.getElementById(idFade2).classList.add("bg-warning")
         }else{
             dataSilang.push(idTarget)
             if(winning()){
                 return
             }
-            targetDiv.classList.add(turn)
         }
 
     }else{
         if(dataLingkaran.length === 3){
             let targetHapus = dataLingkaran[0]
             document.getElementById(targetHapus).classList.remove(turn)
-            document.getElementById(targetHapus).style.opacity = "1"
+            // document.getElementById(targetHapus).style.opacity = "1"
+            document.getElementById(targetHapus).classList.remove("bg-danger")
+            document.getElementById(targetHapus).classList.remove("bg-warning")
             dataLingkaran.shift()
             dataLingkaran.push(idTarget)
             if(winning()){
                 return
             }
             let idFade = dataLingkaran[0]
-            document.getElementById(idFade).style.opacity = "0.5"
+            let idFade2 = dataLingkaran[1]
+            // document.getElementById(idFade).style.opacity = "0.3"
+            document.getElementById(idFade).classList.add("bg-danger")
+            // document.getElementById(idFade2).style.opacity = "0.5"
+            document.getElementById(idFade2).classList.add("bg-warning")
         }else if(dataLingkaran.length === 2){
             dataLingkaran.push(idTarget)
             if(winning()){
                 return
             }
             let idFade = dataLingkaran[0]
-            document.getElementById(idFade).style.opacity = "0.5"
-            targetDiv.classList.add(turn)
+            let idFade2 = dataLingkaran[1]
+            // document.getElementById(idFade).style.opacity = "0.3"
+            document.getElementById(idFade).classList.add("bg-danger")
+            // document.getElementById(idFade2).style.opacity = "0.5"
+            document.getElementById(idFade2).classList.add("bg-warning")
         }else{
             dataLingkaran.push(idTarget)
             if(winning()){
                 return
             }
-            targetDiv.classList.add(turn)
         }
 
     }
+
+
+
+    // let countdown = setInterval(()=>{
+    //     timer--;
+    //     document.getElementById("timer").innerText = timer;
+
+    //     if(timer <= 0){
+    //         clearInterval(countdown);
+    //         win = true;
+    //         winning();
+    //     }
+
+    // },1000)
 
     turn = turn == "silang" ? "lingkaran" : "silang"
 
@@ -143,10 +174,16 @@ function winning(){
 
     if(win){
         console.log(`${turn} winning`)
+        
         arrayInt.forEach(element => {
             let line = document.getElementById(element).classList
+            line.remove("bg-warning")
+            line.remove("bg-danger")
             line.add("bg-success")
         });
+
+        let winning = document.getElementById("turn")
+        winning.innerHTML = `Winner : ${turn}`
         return true
     }
 }
